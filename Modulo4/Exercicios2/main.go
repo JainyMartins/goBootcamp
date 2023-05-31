e948b1240d2533d35b5e7ff802a4b1d64cce5694
JainyMartins <jainy.martins@mercadolivre.com>
2023-05-31 17:49:57 -0300

    Modulo4Tarde

---
+++ Modulo4/Exercicios2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/JainyMartins/goBootcamp/Modulo4/Exercicios2/exercicio1"
+	"github.com/JainyMartins/goBootcamp/Modulo4/Exercicios2/exercicio2"
+)
+
+func main(){
+	fmt.Println("Exercício 2:")
+	exercicio2.Exercicio2()
+	fmt.Println("Exercício 1:")
+	exercicio1.Exercicio1()
+}
